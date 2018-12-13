f5526b444fd98624fd234590e560edd7b02feac2
milan de Ruiter <milanderuiter@macbook-pro-van-milan-2.local>
2018-12-13 15:29:06 +0100

    Initial Commit

---
+++ xcodeserver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcodeserver
+//
+//  Created by milan de Ruiter on 13-12-18.
+//  Copyright © 2018 milan de Ruiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
